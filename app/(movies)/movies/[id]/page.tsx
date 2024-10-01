62a53d7b0f0a01d7127b484b962cbcb5d4730321
seungjunl123 <156068215+seungjunl123@users.noreply.github.com>
2024-10-01 16:55:58 +0900

    second commit

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,7 +1,20 @@
 import { Suspense } from "react";
 import { API_URL } from "../../../(home)/page";
-import MovieInfos from "../../../../components/movie-info";
+import MovieInfos, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
+import { title } from "process";
+
+interface IParams {
+  params: { id: string };
+}
+
+export async function generateMetadata({ params: { id } }: IParams) {
+  const movie = await getMovie(id);
+
+  return {
+    title: movie.title,
+  };
+}
 
 export default async function MovieDetail({
   params: { id },
@@ -10,11 +23,11 @@ export default async function MovieDetail({
 }) {
   return (
     <div>
-      <Suspense fallback={<h1>Loading Movie info</h1>}>
-        <MovieVideos id={id} />
-      </Suspense>
       <Suspense fallback={<h1>Loading Movie Video</h1>}>
         <MovieInfos id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading Movie info</h1>}>
+        <MovieVideos id={id} />
       </Suspense>
     </div>
   );
--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,7 +1,8 @@
 import { API_URL } from "../app/(home)/page";
+import styles from "../styles/movie-info.module.css";
 
-async function getMovie(id: string) {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
+export async function getMovie(id: string) {
+  // await new Promise((resolve) => setTimeout(resolve, 2000));
   //   throw new Error("sometjimg wromg");
 
   const response = await fetch(`${API_URL}/${id}`);
@@ -10,5 +11,17 @@ async function getMovie(id: string) {
 
 export default async function MovieInfos({ id }: { id: string }) {
   const movie = await getMovie(id);
-  return <h6>{JSON.stringify(movie)}</h6>;
+  return (
+    <div className={styles.container}>
+      <img src={movie.poster_path} className={styles.poster} alt="" />
+      <div className={styles.info}>
+        <h1 className={styles.title}>{movie.title}</h1>
+        <h3>{movie.vote_average.toFixed(1)}</h3>
+        <p>{movie.overview}</p>
+        <a href={movie.hompage} target={"_blank"}>
+          HomePage &rarr;
+        </a>
+      </div>
+    </div>
+  );
 }
